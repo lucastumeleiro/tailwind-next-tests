602facb3220c6c10a8ff9f9b11b9e24f10349933
lucas.tumeleiro <lucas@microsys.inf.br>
2023-09-25 15:50:44 -0300

    #feat: focus animation

--- src/components/Input/Control.tsx
+++ src/components/Input/Control.tsx
@@ -5,7 +5,7 @@ type TInputControlProps = ComponentProps<'input'>
 function Control(props: TInputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
       {...props}
     />
   )
