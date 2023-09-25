2d5eb0479187c992d1698e2bd22d43869f9ff440
lucas.tumeleiro <lucas@microsys.inf.br>
2023-09-25 17:47:27 -0300

    #feat: responsive tabs

--- src/components/SettingsTabs/components/TabItems/index.tsx
+++ src/components/SettingsTabs/components/TabItems/index.tsx
@@ -15,7 +15,7 @@ function TabItems({ value, title, isSelected = false }: ITabItemProps) {
       value={value}
       className=" group relative px-1  pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-500 data-[state=active]:text-violet-700"
     >
-      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+      <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
         {title}
       </span>
 
--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import * as Tabs from '@radix-ui/react-tabs'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import { TabItems } from './components/TabItems'
 
 function SettingsTabs() {
@@ -13,58 +14,69 @@ function SettingsTabs() {
 
   return (
     <Tabs.Root value={selectedTab} onValueChange={setSelectedTab}>
-      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        <TabItems
-          value="settings-my-details"
-          title="My details"
-          isSelected={isSelectedTab('settings-my-details')}
-        />
-        <TabItems
-          value="settings-profile"
-          title="Profile"
-          isSelected={isSelectedTab('settings-profile')}
-        />
-        <TabItems
-          value="settings-password"
-          title="Password"
-          isSelected={isSelectedTab('settings-password')}
-        />
-        <TabItems
-          value="settings-Team"
-          title="Team"
-          isSelected={isSelectedTab('settings-Team')}
-        />
-        <TabItems
-          value="settings-Plan"
-          title="Plan"
-          isSelected={isSelectedTab('settings-Plan')}
-        />
-        <TabItems
-          value="settings-Billing"
-          title="Billing"
-          isSelected={isSelectedTab('settings-Billing')}
-        />
-        <TabItems
-          value="settings-Email"
-          title="Email"
-          isSelected={isSelectedTab('settings-Email')}
-        />
-        <TabItems
-          value="settings-Notifications"
-          title="Notifications"
-          isSelected={isSelectedTab('settings-Notifications')}
-        />
-        <TabItems
-          value="settings-Integrations"
-          title="Integrations"
-          isSelected={isSelectedTab('settings-Integrations')}
-        />
-        <TabItems
-          value="settings-API"
-          title="API"
-          isSelected={isSelectedTab('settings-API')}
-        />
-      </Tabs.List>
+      <ScrollArea.Root className="w-full" type="scroll">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+            <TabItems
+              value="settings-my-details"
+              title="My details"
+              isSelected={isSelectedTab('settings-my-details')}
+            />
+            <TabItems
+              value="settings-profile"
+              title="Profile"
+              isSelected={isSelectedTab('settings-profile')}
+            />
+            <TabItems
+              value="settings-password"
+              title="Password"
+              isSelected={isSelectedTab('settings-password')}
+            />
+            <TabItems
+              value="settings-Team"
+              title="Team"
+              isSelected={isSelectedTab('settings-Team')}
+            />
+            <TabItems
+              value="settings-Plan"
+              title="Plan"
+              isSelected={isSelectedTab('settings-Plan')}
+            />
+            <TabItems
+              value="settings-Billing"
+              title="Billing"
+              isSelected={isSelectedTab('settings-Billing')}
+            />
+            <TabItems
+              value="settings-Email"
+              title="Email"
+              isSelected={isSelectedTab('settings-Email')}
+            />
+            <TabItems
+              value="settings-Notifications"
+              title="Notifications"
+              isSelected={isSelectedTab('settings-Notifications')}
+            />
+            <TabItems
+              value="settings-Integrations"
+              title="Integrations"
+              isSelected={isSelectedTab('settings-Integrations')}
+            />
+            <TabItems
+              value="settings-API"
+              title="API"
+              isSelected={isSelectedTab('settings-API')}
+            />
+          </Tabs.List>
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          className="flex h-0.5 translate-y-1.5 touch-none select-none flex-col"
+          orientation="horizontal"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-lg bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </Tabs.Root>
   )
 }
