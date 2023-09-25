cc06679d3ddd476095d89be155a93b9ef285187d
lucas.tumeleiro <lucas@microsys.inf.br>
2023-09-25 14:27:45 -0300

    #fix: input margin

--- src/components/Input/Root.tsx
+++ src/components/Input/Root.tsx
@@ -5,7 +5,7 @@ type TInputRootProps = ComponentProps<'div'>
 function Root(props: TInputRootProps) {
   return (
     <div
-      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
       {...props}
     />
   )
