fed9b308813816e39c9beccb4decbf5e9dd07bde
lucas.tumeleiro <lucas@microsys.inf.br>
2023-09-25 15:36:35 -0300

    feat: test auto-animate lib

--- src/components/FileInput/FileList.tsx
+++ src/components/FileInput/FileList.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { Trash2, UploadCloud } from 'lucide-react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { useFileInput } from './Root'
 import { formatBytes } from '@/Utils/functions'
 
 function FileList() {
   const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
 
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
         console.log('file:', file)
         return (
